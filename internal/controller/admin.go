ff0a58ae0462ae3a5c1e937e6e1fca3ca9e971ad
Marc Peral <m.peral@sapalomera.cat>
2024-04-17 16:58:50 +0200

    Refactor authentication and session handling code in auth.go and middleware/auth.go

--- internal/controller/admin.go
+++ internal/controller/admin.go
@@ -2,10 +2,22 @@ package controller
 
 import (
 	"net/http"
+	"ovo-server/internal/session"
+	"ovo-server/internal/template/page"
 
 	"github.com/labstack/echo/v4"
 )
 
 func AdminDashboard(context echo.Context) error {
-	return context.String(http.StatusOK, "Admin Dashboard")
+	pageData := page.AdminDashboardPageData{
+		Username: session.GetUserSession(context).Username,
+	}
+	component := page.AdminDashboardPage(pageData)
+	return RenderView(context, http.StatusOK, component)
+}
+
+func AdminLibraries(context echo.Context) error {
+	// pageData := page.LibrariesPageData{}
+	component := page.LibrariesPage()
+	return RenderView(context, http.StatusOK, component)
 }
--- internal/controller/home.go
+++ internal/controller/home.go
@@ -2,14 +2,17 @@ package controller
 
 import (
 	"net/http"
+	"ovo-server/internal/session"
 	"ovo-server/internal/template/page"
 
 	"github.com/labstack/echo/v4"
 )
 
 func Home(c echo.Context) error {
+	userSession := session.GetUserSession(c)
+
 	pageData := page.HomePageData{
-		Username: "user",
+		Username: userSession.Username,
 	}
 	component := page.HomePage(pageData)
 	return RenderView(c, http.StatusOK, component)
