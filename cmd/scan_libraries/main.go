da3e85085573dc85152af06c31d4728ccad74030
Marc Peral <m.peral@sapalomera.cat>
2024-05-01 18:56:22 +0200

    Add config and TMDB API settings to test library scan

--- cmd/scan_libraries/main.go
+++ cmd/scan_libraries/main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"log"
+	"ovo-server/internal/config"
 	"ovo-server/internal/database"
 	"ovo-server/internal/model"
+	"ovo-server/internal/tmdb"
 )
 
 func main() {
-
+	config.Init()
+	tmdb.Init()
 	database.Init()
 
 	libraries := model.GetLibraries()
 
 	for _, library := range libraries {
+		log.Printf("Scanning library: %s in %s\n", library.Name, library.Paths)
 		library.ScanLibrary()
 	}
 
