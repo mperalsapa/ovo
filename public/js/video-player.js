950c312bcaa718cb2313ef4b3c05f0d5b7a641e4
Marc Peral <m.peral@sapalomera.cat>
2024-05-14 17:30:47 +0200

    refactor: Improve time formatting in video player

--- public/js/video-player.js
+++ public/js/video-player.js
@@ -130,7 +130,7 @@ export class VideoPlayer {
         let hours = Math.floor(time / 3600);
         let minutes = Math.floor(time % 3600 / 60);
         let seconds = Math.floor(time % 3600 % 60);
-        return `${hours}:${minutes}:${seconds.toString().padStart(2, '0')}`;
+        return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
     }
 
     VolumeUp() {
